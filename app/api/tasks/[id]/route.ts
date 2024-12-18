9c7ec3066183f1d1889ee60afbafa18353d5f9b4
Raul-TC <raultc.9819@gmail.com>
2024-12-18 16:23:28 -0600

    .

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -96,7 +96,7 @@ export async function DELETE(req: Request, { params }: { params: { id?: string }
     }
 }
 
-export async function GET({ params }: { params: { id?: string } }) {
+export async function GET(req: Request, { params }: { params: { id?: string } }) {
 
 
 
