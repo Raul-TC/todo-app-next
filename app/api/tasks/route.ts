cb86fe5fe3ff87babebc3c2b60cb05814816ef4f
Raul-TC <raultc.9819@gmail.com>
2024-12-26 06:52:00 -0600

    remove sesion

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,4 +1,4 @@
-import { auth } from "@/auth";
+// import { auth } from "@/auth";
 import prisma from "@/libs/db";
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
@@ -6,14 +6,14 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
 
     try {
-        const session = await auth()
+        // const session = await auth()
         const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
         console.log('OBTENIENDNO TASKS ')
         console.log({ token })
-        if (!session || !session.user.id) {
-            return NextResponse.json({ message: 'No autorizado' }, { status: 401 });
-        }
+        // if (!session || !session.user.id) {
+        //     return NextResponse.json({ message: 'No autorizado' }, { status: 401 });
+        // }
 
         // if (params.id !== session?.user.id) {
         //     return NextResponse.json({ message: 'No tienes permiso para acceder a estas tareas' }, { status: 403 });
@@ -43,21 +43,22 @@ export async function POST(req: Request) {
 
 
     try {
-        const session = await auth()
-        if (!session || !session.user.id) {
-            return NextResponse.json({ message: 'No autorizado' }, { status: 401 });
-        }
+        // const session = await auth()
+        // if (!session || !session.user.id) {
+        //     return NextResponse.json({ message: 'No autorizado' }, { status: 401 });
+        // }
 
         // if (params.id !== session?.user.id) {
         //     return NextResponse.json({ message: 'No tienes permiso para acceder a estas tareas' }, { status: 403 });
         // }
-        const { content }: { content: string } = await req.json()
+        const { content, userId }: { content: string, userId: string } = await req.json()
         const newTask = await prisma.task.create({
             data: {
                 content,
                 isDone: false,
                 isNew: true,
-                userId: session.user.id
+                userId: userId
+                // userId: session.user.id
             }
         })
         return NextResponse.json({ message: 'Task Created', newTask }, { status: 201 })
