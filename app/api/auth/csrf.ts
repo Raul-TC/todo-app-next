2509d10c2d8114ffd4d73027bcfb7571d1bf66a1
Raul-TC <raultc.9819@gmail.com>
2025-02-06 12:41:10 -0600

    fix Home

--- app/api/auth/csrf.ts
+++ app/api/auth/csrf.ts
@@ -1,27 +1,27 @@
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export async function handler(req: NextRequest) {
-    if (req.method === 'GET') {
-        // Respuesta exitosa para GET
-        return NextResponse.json(
-            {
-                success: true,
-                message: 'CSRF token retrieved successfully.',
-            },
-            { status: 200 }
-        );
-    }
+// export async function handler(req: NextRequest) {
+//     if (req.method === 'GET') {
+//         // Respuesta exitosa para GET
+//         return NextResponse.json(
+//             {
+//                 success: true,
+//                 message: 'CSRF token retrieved successfully.',
+//             },
+//             { status: 200 }
+//         );
+//     }
 
-    // Manejo de error para POST u otros métodos
-    return NextResponse.json(
-        {
-            error: 'Method not allowed',
-            message: 'Only GET requests are allowed on this endpoint.',
-        },
-        { status: 405 }
-    );
-}
+//     // Manejo de error para POST u otros métodos
+//     return NextResponse.json(
+//         {
+//             error: 'Method not allowed',
+//             message: 'Only GET requests are allowed on this endpoint.',
+//         },
+//         { status: 405 }
+//     );
+// }
 
-export const config = {
-    runtime: 'edge', // Opcional si estás usando Edge Runtime
-};
+// export const config = {
+//     runtime: 'edge', // Opcional si estás usando Edge Runtime
+// };
--- app/page.tsx
+++ app/page.tsx
@@ -10,20 +10,18 @@ export default async function Home() {
   const session = await auth()
 
 
-  console.log({ sessionFromLogin: session })
   if (!session) redirect('/auth/login')
   const { getAllTasks } = crudActions()
 
   const tasks = await getAllTasks(session)
 
-  console.log(typeof tasks)
+  console.log({ tasks })
   return (
     <Suspense key={crypto.randomUUID()} fallback={<TaskSkeleton />}>
       {
-        !tasks.error?.isError ? <TodoList session={session} tasks={tasks} />
-          : <div className='min-h-[calc(100vh-308px)] flex'>
-            <h1 className='text-center m-auto text-3xl'>{tasks.error?.message.toString()}</h1>
-          </div>
+        <>
+          <TodoList session={session} tasks={tasks} />
+        </>
       }
     </Suspense>
   )
